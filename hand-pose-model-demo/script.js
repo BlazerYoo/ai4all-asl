11b23598401033ff686c7257ddb6727f4fe80cc9
BlazerYoo <blazeryoo1921@gmail.com>
2024-07-31 21:56:22 +0000

    show prediction

--- hand-pose-model-demo/script.js
+++ hand-pose-model-demo/script.js
@@ -2,23 +2,10 @@
 const sess = new onnx.InferenceSession();
 const loadingModelPromise = sess.loadModel('onnx_model.onnx');
 
-const captureSpeed = document.getElementById('captureSpeed');
-const letterImage = document.getElementById('letterImage');
 const captureButton = document.getElementById('captureButton');
-//const saveButton = document.getElementById('saveButton');
+const predictionText = document.getElementById('prediction');
 
-let captureInterval;
 let handsKeypoints;
-
-// Show hand sign
-label.addEventListener('change', (event) => {
-    const selectedValue = event.target.value;
-    if (selectedValue !== '-') {
-        letterImage.src = `assets/${selectedValue}.jpeg`;
-    } else {
-        letterImage.src = '';
-    }
-});
 
 // Get predictions
 
@@ -63,39 +50,30 @@ async function updatePredictions(keypointsArr) {
   const outputTensor = outputMap.values().next().value;
   const predictions = outputTensor.data;
 
-  console.log(get_letter(argMax(predictions)));
+  const prediction = get_letter(argMax(predictions));
+
+  predictionText.innerText = prediction;
 }
 
 // Get poses
-captureButton.addEventListener('mousedown', () => {
-  captureInterval = setInterval(() => {
-    if (handsKeypoints.length !== 0) {
+captureButton.addEventListener('click', () => {
+  if (handsKeypoints.length !== 0) {
 
-      const handsKeypoints2D = handsKeypoints[0].keypoints3D;
-      //console.log(handsKeypoints2D);
+    const handsKeypoints2D = handsKeypoints[0].keypoints3D;
+    //console.log(handsKeypoints2D);
 
-      // Convert to array
-      const keypointsArr = new Array();
-      handsKeypoints2D.map((coord) => {
-        keypointsArr.push(coord.x);
-        keypointsArr.push(coord.y);
-      });
-      //console.log(keypointsArr);
+    // Convert to array
+    const keypointsArr = new Array();
+    handsKeypoints2D.map((coord) => {
+      keypointsArr.push(coord.x);
+      keypointsArr.push(coord.y);
+    });
+    //console.log(keypointsArr);
 
-      // Make prediction
-      updatePredictions(keypointsArr);
-    }
-    //console.log(`x: ${handsKeypoints[0].keypoints3D.map((kp) => kp.x)}`);
-    //console.log(`y: ${handsKeypoints[0].keypoints3D.map((kp) => kp.y)}`);
-    //console.log(`z: ${handsKeypoints[0].keypoints3D.map((kp) => kp.z)}`);
-  }, captureSpeed.value); // Capture frame every Xms
-});
-
-// Stop pose collection
-captureButton.addEventListener('mouseup', () => {
-  clearInterval(captureInterval);
-});
-
-captureButton.addEventListener('mouseleave', () => {
-  clearInterval(captureInterval);
+    // Make prediction
+    updatePredictions(keypointsArr);
+  }
+  //console.log(`x: ${handsKeypoints[0].keypoints3D.map((kp) => kp.x)}`);
+  //console.log(`y: ${handsKeypoints[0].keypoints3D.map((kp) => kp.y)}`);
+  //console.log(`z: ${handsKeypoints[0].keypoints3D.map((kp) => kp.z)}`);
 });
