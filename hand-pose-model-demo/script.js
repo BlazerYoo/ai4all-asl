4538891af8bed84ee0c94dd1a1e44aaeb86d505d
BlazerYoo <blazeryoo1921@gmail.com>
2024-07-31 20:18:54 +0000

    use onnx model export

--- hand-pose-model-demo/script.js
+++ hand-pose-model-demo/script.js
@@ -16,13 +16,59 @@ label.addEventListener('change', (event) => {
     }
 });
 
-// Collect poses
+// Get predictions
+
+function argMax(array) {
+  return [].map.call(array, (x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+function get_letter(index) {
+  const mapping = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D",
+    4: "E",
+    5: "F",
+    6: "G",
+    7: "H",
+    8: "I",
+    9: "K",
+    10: "L",
+    11: "M",
+    12: "N",
+    13: "O",
+    14: "P",
+    15: "Q",
+    16: "R",
+    17: "S",
+    18: "T",
+    19: "U",
+    20: "V",
+    21: "W",
+    22: "X",
+    23: "Y",
+  };
+  return mapping[index];
+}
+
+async function updatePredictions() {
+  const input = new onnx.Tensor(new Float32Array(imgData.data), "float32", [1, 42]);
+
+  const outputMap = await sess.run([input]);
+  const outputTensor = outputMap.values().next().value;
+  const predictions = outputTensor.data;
+
+  console.log(get_letter(argMax(predictions)));
+}
+
+// Get poses
 captureButton.addEventListener('mousedown', () => {
   captureInterval = setInterval(() => {
     if (handsKeypoints.length !== 0) {
 
-      const handsKeypoints2D = handsKeypoints[0].keypoints;
-      console.log(handsKeypoints2D);
+      const handsKeypoints2D = handsKeypoints[0].keypoints3D;
+      //console.log(handsKeypoints2D);
 
       // Convert to array
       const keypointsArr = new Array();
@@ -30,7 +76,10 @@ captureButton.addEventListener('mousedown', () => {
         keypointsArr.push(coord.x);
         keypointsArr.push(coord.y);
       });
-      console.log(keypointsArr);
+      //console.log(keypointsArr);
+
+      // Make prediction
+      updatePredictions(keypointsArr);
     }
     //console.log(`x: ${handsKeypoints[0].keypoints3D.map((kp) => kp.x)}`);
     //console.log(`y: ${handsKeypoints[0].keypoints3D.map((kp) => kp.y)}`);
